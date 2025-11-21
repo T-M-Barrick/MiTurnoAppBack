611478c1756924f129d07d72e8cec910a331f2d9
tomas.r.9090gmail.com <tomas.r.9090gmail.com>
2025-11-20 21:54:38 -0300

    MiTurno 2.2

--- core/variables.py
+++ core/variables.py
@@ -25,7 +25,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-DB_URL = f'mysql+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
