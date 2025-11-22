fc637e5b34d813fa06721c39ad38fa45427d2244
tomas.r.9090gmail.com <tomas.r.9090gmail.com>
2025-11-21 22:33:57 -0300

    MiTurno 2.5.3

--- core/variables.py
+++ core/variables.py
@@ -21,11 +21,11 @@ EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_APP_PASSWORD = os.getenv("EMAIL_APP_PASSWORD")
 FRONTEND_URL = os.getenv("FRONTEND_URL")
 
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "zaUbBvZNnNWDUhKqBnkDqlQlwrmCmwTu")
-DB_HOST = os.getenv("DB_HOST", "mysql-gwzs.railway.internal")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
-DB_NAME = os.getenv("DB_NAME", "railway")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT"))
+DB_NAME = os.getenv("DB_NAME")
 
 DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
