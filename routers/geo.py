a5f56492b68e4d4e7dc1b6dbda36fa46e893efd5
tomas.r.9090gmail.com <tomas.r.9090gmail.com>
2025-11-22 13:08:38 -0300

    MiTurno 2.5.4

--- routers/geo.py
+++ routers/geo.py
@@ -10,7 +10,7 @@ def get_provincias():
     r = requests.get(f"{GEOREF_URL}/provincias")
     return r.json()["provincias"]
 
-@router.get("/departamentos/{provincia}")
+@router.get("/departamentos")
 def get_departamentos(provincia: str):
     r = requests.get(f"{GEOREF_URL}/departamentos", params={"provincia": provincia, "max": 500})
     return r.json()["departamentos"]
@@ -21,10 +21,24 @@ def get_localidades(provincia: str, municipio: str):
     return r.json()["localidades"]
 
 @router.get("/coordenadas")
-def get_coordenadas(calle: str, altura: str, provincia: str):
+def get_coordenadas(provincia: str, municipio: str, localidad: str, calle: str, altura: str):
     direccion = f"{calle} {altura}"
-    r = requests.get(f"{GEOREF_URL}/direcciones", params={"direccion": direccion, "provincia": provincia})
+    r = requests.get(f"{GEOREF_URL}/direcciones", params={"provincia": provincia, "municipio": municipio,
+            "localidad": localidad, "direccion": direccion, "max": 5})
     data = r.json()
     if data["cantidad"] == 0:
+        return None   
+    return data["direcciones"][0]  # devuelve lat/long y datos normalizados
+
+@router.get("/reverse")
+def reverse_geocode(lat: float, lng: float):
+    url = "https://nominatim.openstreetmap.org/reverse"
+    params = {"format": "json", "lat": lat, "lon": lng, "addressdetails": 1}
+
+    r = requests.get(url, params=params, headers={"User-Agent": "MiApp"})
+    data = r.json()
+
+    if "display_name" not in data:
         return None
-    return data["direcciones"][0]  # devuelve lat/long
+
+    return {"domicilio": data["display_name"]}
