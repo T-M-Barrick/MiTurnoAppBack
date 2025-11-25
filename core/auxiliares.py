e8514a0bd2e227c69183f096be691d5e1efab3e2
tomas.r.9090gmail.com <tomas.r.9090gmail.com>
2025-11-25 02:16:15 -0300

    MiTurno 2.7.1

--- core/auxiliares.py
+++ core/auxiliares.py
@@ -252,6 +252,9 @@ def buscar_direccion_completa(provincia: str, municipio: str, localidad: str, ca
 
         r = requests.get(url, params=params, timeout=5, headers={"User-Agent": "MiApp/1.0"})
 
+        print(r.status_code)
+        print(r.json())  # <--- imprime todo el JSON
+
         if r.status_code != 200:
             return {"error": "Fallo en la API", "detalle": r.text}
 
